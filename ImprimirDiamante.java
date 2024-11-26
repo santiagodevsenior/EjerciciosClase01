8799dca7b6217595b3f79accbfe39dcdf85e1b60
Angel Santiago Zarta Sarmiento <angelsarmientozarta@gmail.com>
2024-11-26 18:01:33 -0500

    new file:   ImprimirDiamante.class 	new file:   ImprimirDiamante.java

---
+++ ImprimirDiamante.java
@@ -0,0 +1,14 @@
+public class ImprimirDiamante {
+    public static void main(String[] args) {
+        System.out.println("---------------");
+        System.out.println("   Diamond     ");
+        System.out.println("---------------");
+        System.out.println("      *       ");
+        System.out.println("     ***      ");
+        System.out.println("    *****     ");
+        System.out.println("   *******    ");
+        System.out.println("    *****     ");
+        System.out.println("     ***      ");
+        System.out.println("      *       ");
+    }
+}
